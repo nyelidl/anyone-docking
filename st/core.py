1ad4711e34c2e743018d491731da3d3fd4a173a5
nyelidl <120070650+nyelidl@users.noreply.github.com>
2026-07-03 16:23:21 +0900

    Update core.py

--- st/core.py
+++ st/core.py
@@ -46,7 +46,7 @@ COFACTOR_NAMES = {
     "GOL", "PEG", "EDO", "MPD", "PGE", "PG4",
     "SO4", "PO4", "SUL", "PHO",
     "IHP", "TTP", "CTP", "UTP",
-    "COA", "SAM", "SAH",
+    #"COA", "SAM", "SAH",
     "EPE", "MES", "TRS", "ACT", "ACY",
     "HO", "LA", "CE", "PR", "ND", "PM", "SM", "EU", "GD", "TB", "DY", "ER", "TM", "YB", "LU",
 }
